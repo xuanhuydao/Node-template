ecf1ebfdb6f582c79f538f9f9712b51f57e45612
XuanHuy <daoxuanhuy2004zz@gmail.com>
2025-08-07 15:17:52 +0700

    init project basic

---
+++ server.js
@@ -0,0 +1,13 @@
+const app = require("./src/app");
+
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+process.on("SIGINT", () => {
+  server.close(() => {
+    console.log("Server express exit");
+  })
+});
---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
+const app = express();
+
+//init middlewares
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(compression());
+
+//init db
+
+//init routes
+app.get('/', (req, res, next) => {
+  const strCompress = "hello world";
+  return res.status(200).json({
+    message: "welcome to Node-template",
+    metadata: strCompress.repeat(100000)
+  })
+});
+//handling errors
+
+module.exports = app;
