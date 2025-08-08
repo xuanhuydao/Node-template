3954b535c51ed45aaf1b543dec31021e789716a9
XuanHuy <daoxuanhuy2004zz@gmail.com>
2025-08-08 15:07:06 +0700

    fix configs

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const app = require("./src/app");
 
-const PORT = process.env.PORT || 3000;
+const {app : {port}} = require('./src/configs/config.mongodb')
 
-const server = app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+const server = app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
 });
 
 process.on("SIGINT", () => {
--- src/configs/config.mongodb.js
+++ src/configs/config.mongodb.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
+
 const dev = {
     app: {
         port: process.env.DEV_APP_PORT || 3052
     },
     db: {
         host: process.env.DEV_DB_HOST || 'localhost',
-        port: process.env.DEV_DB_PORT || 27107,
+        port: process.env.DEV_DB_PORT || 27017,
         name: process.env.DEV_DB_NAME || 'shopDev'
     }
 }
